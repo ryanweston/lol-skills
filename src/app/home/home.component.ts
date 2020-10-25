37fe0f233778d4b9b8d2a12747d004315b6b2d79
ryanlweston <ryanlweston@gmail.com>
2020-10-25 12:01:37 +0000

    home style updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,15 +4,20 @@ import { Router, NavigationExtras } from '@angular/router';
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
 
+    hoverStatus = [];
     data = [{ id: 'skills', status: true }, { id: 'champions', status: false }, { id: 'items', status: false }];
-    selected = String;
+    selected = '';
 
     constructor(private router: Router) {
 
     }
 
     ngOnInit() {
-
+        //Set hover states
+        for (let i = 0; i < this.data.length; i++) {
+            this.hoverStatus.push(false);
+        }
+        console.log(this.hoverStatus);
     }
 
     verify() {
@@ -25,6 +30,15 @@ export class HomeComponent {
     select(type) {
         this.selected = type;
         console.log(this.selected);
+    }
+
+    mouseEnter(id) {
+        console.log("mouse enter : " + id);
+        this.hoverStatus[id] = true;
+    }
+    mouseLeave(id) {
+        this.hoverStatus[id] = false;
+        console.log("mouse leave : " + id);
     }
 
 }
