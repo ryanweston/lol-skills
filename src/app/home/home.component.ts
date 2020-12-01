d90a3026b3a4ef641615464c9487bd34d1aed1b0
Ryan Weston <ryanlweston@gmail.com>
2020-12-01 20:46:01 +0000

    Mass commit: home page overhaul + change of font

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 
 @Component({ templateUrl: 'home.component.html', styleUrls: ['home.component.scss'] })
+
+
 export class HomeComponent {
 
+    constructor(private router: Router) {
+    }
+
     hoverStatus = [];
-    data = [{ id: 'skills', status: true }, { id: 'champions', status: false }, { id: 'items', status: false }];
+    navScroll = false;
+    data = [{ id: 'skills', status: true }, { id: 'champions', status: false }, { id: 'items', status: false }, { id: 'objectives', status: false }, { id: 'counters', status: false }];
     selected = '';
 
-    constructor(private router: Router) {
 
-    }
 
     ngOnInit() {
         //Set hover states
@@ -41,5 +45,29 @@ export class HomeComponent {
     mouseLeave(index) {
         this.hoverStatus[index] = false;
     }
+  //
+  //   onScroll() {
+  //     console.log("CALLED");
+  //     //In chrome and some browser scroll is given to body tag
+  //     let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+  //     let max = document.documentElement.scrollHeight;
+  //     console.log(pos);
+  //     console.log(max);
+  //       // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+  //      if(pos == max )   {
+  //        console.log("pos is max");
+  //      //Do your action here
+  //      }
+  // }
+  onWindowScroll() {
+    console.log(document.documentElement.scrollTop);
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    let max = document.documentElement.scrollHeight;
+    if(pos >= (max + 100)) {
+      this.navScroll = true;
+    } else {
+      this.navScroll = false;
+    }
+}
 
 }
