1eae5d46a30063667a2bf7644f1cd0224d4f17dd
ryanlweston <ryanlweston@gmail.com>
2020-12-02 23:54:58 +0000

    reorganised order of categories + added menu title

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent {
 
     hoverStatus = [];
     navScroll = false;
-    data = [{ id: 'skills', status: true }, { id: 'champions', status: false }, { id: 'items', status: false }, { id: 'objectives', status: false }, { id: 'counters', status: false }];
+    data = [{ id: 'skills', status: true }, { id: 'champions', status: false }, { id: 'objectives', status: false }, { id: 'counters', status: false }, { id: 'items', status: false }];
     selected = '';
 
 
@@ -45,29 +45,29 @@ export class HomeComponent {
     mouseLeave(index) {
         this.hoverStatus[index] = false;
     }
-  //
-  //   onScroll() {
-  //     console.log("CALLED");
-  //     //In chrome and some browser scroll is given to body tag
-  //     let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
-  //     let max = document.documentElement.scrollHeight;
-  //     console.log(pos);
-  //     console.log(max);
-  //       // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
-  //      if(pos == max )   {
-  //        console.log("pos is max");
-  //      //Do your action here
-  //      }
-  // }
-  onWindowScroll() {
-    console.log(document.documentElement.scrollTop);
-    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
-    let max = document.documentElement.scrollHeight;
-    if(pos >= (max + 100)) {
-      this.navScroll = true;
-    } else {
-      this.navScroll = false;
+    //
+    //   onScroll() {
+    //     console.log("CALLED");
+    //     //In chrome and some browser scroll is given to body tag
+    //     let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    //     let max = document.documentElement.scrollHeight;
+    //     console.log(pos);
+    //     console.log(max);
+    //       // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+    //      if(pos == max )   {
+    //        console.log("pos is max");
+    //      //Do your action here
+    //      }
+    // }
+    onWindowScroll() {
+        console.log(document.documentElement.scrollTop);
+        let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+        let max = document.documentElement.scrollHeight;
+        if (pos >= (max + 100)) {
+            this.navScroll = true;
+        } else {
+            this.navScroll = false;
+        }
     }
-}
 
 }
